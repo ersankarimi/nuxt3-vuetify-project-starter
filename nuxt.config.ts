13f5596567c01e6b8c74651b1f877f61700f239b
Ersan Karimi <ersankarimii311@gmail.com>
2024-05-14 14:46:05 +0700

    chore: Update nuxt.config.ts to disable server-side rendering (SSR)
    This commit updates the nuxt.config.ts file to disable server-side rendering (SSR) by setting the "ssr" property to false. This change was made to improve the performance of the application.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
+
 export default defineNuxtConfig({
+  ssr: false,
   devtools: { enabled: true },
-  modules: [],
+  build: {
+    transpile: ["vuetify"],
+  },
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook("vite:extendConfig", (config) => {
+        // eslint-disable-next-line ts/ban-ts-comment
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }));
+      });
+    },
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
 
 });
